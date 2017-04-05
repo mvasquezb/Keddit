d5a1f4a48814121ce3b256a4cfc8c19947df6060
Martín Vásquez <pedromvasquezb@gmail.com>
2017-04-04 22:27:00 -0500

    Modify MainActivity to use NewsFragment

--- app/src/main/java/com/pmvb/keddit/MainActivity.kt
+++ app/src/main/java/com/pmvb/keddit/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.widget.Toolbar
+import com.pmvb.keddit.features.news.NewsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+
+        if (savedInstanceState == null) {
+            changeFragment(NewsFragment())
+        }
     }
 
     fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
