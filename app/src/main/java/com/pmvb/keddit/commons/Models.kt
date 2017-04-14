d01d9b7812d94603c2788de347f777a65a22cbb1
Martín Vásquez <pedromvasquezb@gmail.com>
2017-04-13 19:46:14 -0500

    Add 'ViewType' to 'RedditNewsItem'

--- app/src/main/java/com/pmvb/keddit/commons/Models.kt
+++ app/src/main/java/com/pmvb/keddit/commons/Models.kt
@@ -1,5 +1,8 @@
 package com.pmvb.keddit.commons
 
+import com.pmvb.keddit.commons.adapter.AdapterConstants
+import com.pmvb.keddit.commons.adapter.ViewType
+
 data class RedditNewsItem(
         val author: String,
         val title: String,
@@ -7,4 +10,6 @@ data class RedditNewsItem(
         val created: Long,
         val thumbnail: String,
         val url: String
-)
+): ViewType {
+    override fun getViewType() = AdapterConstants.NEWS
+}
