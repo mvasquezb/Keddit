f71e59da5ebdb113ca12d83f7fa91489243d5dae
Martín Vásquez <pedromvasquezb@gmail.com>
2017-04-23 12:39:04 -0500

    Add 'RedditNewsPage' model

--- app/src/main/java/com/pmvb/keddit/commons/Models.kt
+++ app/src/main/java/com/pmvb/keddit/commons/Models.kt
@@ -3,6 +3,12 @@ package com.pmvb.keddit.commons
 import com.pmvb.keddit.commons.adapter.AdapterConstants
 import com.pmvb.keddit.commons.adapter.ViewType
 
+data class RedditNewsPage(
+        val after: String,
+        val before: String,
+        val news: List<RedditNewsItem>
+)
+
 data class RedditNewsItem(
         val author: String,
         val title: String,
