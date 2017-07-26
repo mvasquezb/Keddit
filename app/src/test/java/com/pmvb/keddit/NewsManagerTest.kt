0a0483acda705d7dd6b7983b01a8b8d81dddbc13
Martín Vásquez <pedromvasquezb@gmail.com>
2017-07-26 15:38:12 -0500

    Add some basic tests

--- app/src/test/java/com/pmvb/keddit/NewsManagerTest.kt
+++ app/src/test/java/com/pmvb/keddit/NewsManagerTest.kt
@@ -1,18 +1,19 @@
 package com.pmvb.keddit
 
-import com.pmvb.keddit.api.NewsAPI
-import com.pmvb.keddit.api.RedditDataResponse
-import com.pmvb.keddit.api.RedditNewsResponse
+import com.pmvb.keddit.api.*
 import com.pmvb.keddit.commons.RedditNewsPage
 import com.pmvb.keddit.features.news.NewsManager
 import io.reactivex.observers.TestObserver
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Matchers
+import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.any
+import okhttp3.MediaType
+import okhttp3.ResponseBody
 import org.mockito.Mockito
-import org.mockito.Mockito.`when`
 import retrofit2.Call
 import retrofit2.Response
+import java.util.*
 
 inline fun <reified T : Any> mock(): T = Mockito.mock(T::class.java)
 
@@ -26,7 +27,7 @@ class NewsManagerTest {
         testSub = TestObserver<RedditNewsPage>()
         mockAPI = mock<NewsAPI>()
         mockCall = mock<Call<RedditNewsResponse>>()
-        `when`(mockAPI.getNews(Matchers.anyString(), Matchers.anyInt())).thenReturn(mockCall)
+        whenever(mockAPI.getNews(any(), any())).thenReturn(mockCall)
     }
 
     @Test
@@ -35,7 +36,7 @@ class NewsManagerTest {
         val redditNewsResponse = RedditNewsResponse(RedditDataResponse(listOf(), null, null))
         val response = Response.success(redditNewsResponse)
 
-        `when`(mockCall.execute()).thenReturn(response)
+        whenever(mockCall.execute()).thenReturn(response)
 
         // Call
         val newsManager = NewsManager(mockAPI)
@@ -45,5 +46,52 @@ class NewsManagerTest {
         testSub.assertNoErrors()
         testSub.assertValueCount(1)
         testSub.assertComplete()
+    }
+
+    @Test
+    fun testSuccess_checkOneNews() {
+        // Preparation
+        val newsData = RedditNewsDataResponse(
+                "author",
+                "title",
+                10,
+                Date().time,
+                "thumbnail",
+                "url"
+        )
+        val newsResponse = RedditChildrenResponse(newsData)
+        val redditNewsResponse = RedditNewsResponse(
+                RedditDataResponse(listOf(newsResponse), null, null))
+        val response = Response.success(redditNewsResponse)
+
+        whenever(mockCall.execute()).thenReturn(response)
+
+        // Call
+        val newsManager = NewsManager(mockAPI)
+        newsManager.getNews("").subscribe(testSub)
+
+        // Assertion
+        testSub.assertNoErrors()
+        testSub.assertValueCount(1)
+        testSub.assertComplete()
+        assert((testSub.events[0][0] as RedditNewsPage).news[0].author == newsData.author)
+        assert((testSub.events[0][0] as RedditNewsPage).news[0].title == newsData.title)
+    }
+
+    @Test
+    fun testError() {
+        // Preparation
+        val responseError = Response.error<RedditNewsResponse>(
+                500,
+                ResponseBody.create(MediaType.parse("application/json"), "")
+        )
+        whenever(mockCall.execute()).thenReturn(responseError)
+
+        // Call
+        val newsManager = NewsManager(mockAPI)
+        newsManager.getNews("").subscribe(testSub)
+
+        // Assertion
+        assert(testSub.errorCount() == 1)
     }
 }
